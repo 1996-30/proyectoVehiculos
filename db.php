48b1b5ce38c64521bbe70cf7c5eae01dd90731a4
Heber Monzon <hebermonzon@hotmail.com>
2024-10-06 11:27:15 -0600

    Catalogos Reserva Vehiculos

---
+++ db.php
@@ -0,0 +1,20 @@
+<?php
+
+$host = "localhost";
+$user = "root";
+$pass = "";
+$db = "renta_vehiculos";
+
+
+
+$conn = new mysqli($host, $user, $pass, $db);
+
+//echo $conn;
+
+/*if(isset($conn)){
+    echo "Conexion exitosa";
+}else{
+    echo "Conexion sin Exito";
+}*/
+
+?>
